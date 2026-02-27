82c85025db655027e74d3cc73ec162a90af5b7ae
jhwalters99 <jhwalters99@gmail.com>
2026-02-27 16:05:42 -0600

    refactored getLocation function

--- src/index.js
+++ src/index.js
@@ -12,47 +12,30 @@ myLocationBtn.addEventListener('click', getLocation);
 
 changeLocationBtn.addEventListener('click', () => {
     const newCity = prompt("Enter a city name:");
-    if (newCity) {
-        cityName.textContent = newCity;
-    }
-    fetchWeatherData(newCity);
+    if (newCity) fetchWeatherData(newCity);
 });
 
 function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-        
-    } else {
+    if (!navigator.geolocation) {
         alert("Geolocation is not supported by this browser.");
+        return;
     }
+    navigator.geolocation.getCurrentPosition(
+        position => {
+            const { latitude, longitude } = position.coords;
+            fetchWeatherData(`${latitude},${longitude}`);
+        },
+        handleLocationError
+    );
 }
 
-function showPosition(position) {
-    const lat = position.coords.latitude;
-    const lon = position.coords.longitude;
-    cityName.textContent = `Lat: ${lat.toFixed(2)}, Lon: ${lon.toFixed(2)}`;
-    console.log(`Latitude: ${lat}, Longitude: ${lon}`);
-    fetchWeatherData(`${lat},${lon}`);
+function handleLocationError(error) {
+    alert(error.message);
 }
 
-function showError(error) {
-    switch (error.code) {
-        case error.PERMISSION_DENIED:
-            alert("User denied the request for Geolocation.");
-            break;
-        case error.POSITION_UNAVAILABLE:
-            alert("Location information is unavailable.");
-            break;
-        case error.TIMEOUT:
-            alert("The request to get user location timed out.");
-            break;
-        case error.UNKNOWN_ERROR:
-            alert("An unknown error occurred.");
-            break;
-    }
-}
-
-function fetchWeatherData(city) {
+async function fetchWeatherData(city) {
+    const currentUrl = `https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${query}&aqi=no`;
+    const forecastUrl = `https://api.weatherapi.com/v1/forecast.json?key=${API_KEY}&q=${query}&days=7&aqi=no&alerts=no`;
     const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no
 `;
     fetch(apiUrl)
